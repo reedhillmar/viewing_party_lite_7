1d5889ca6198e9bf5633d02e0cfd879d39e318bd
Reed Hillmar <reedhillmar@gmail.com>
2023-10-23 16:18:16 -0600

    adds password_confirmation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,5 +13,6 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email).case_insensitive }
     it { should validate_presence_of(:password) }
+    it { should have_secure_password }
   end
 end
